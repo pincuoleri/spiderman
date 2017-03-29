e2b6aae90f6f489decc189ef590b288a63dc706c
jkwang <jkwang@jkwangdeMac.local>
2017-03-29 17:11:10 +0800

    alpha

--- JiandanSpider.py
+++ JiandanSpider.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import re, requests
+import re, requests, os
 #without threading
 
 RequestsHeaders= {'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -21,4 +21,43 @@ def savepic(picpath, picname, picinfo):
             if i:
                 f.write(i)
 
-def spiderman()
+
+#get 无聊图图片(picinfolist用来收集信息，最后返回这个列表，这么做不为别的，就是想调试时候方便点)
+def spiderman(page):
+        url= 'http://jandan.net/pic/page-'+str(page)
+        picinfolist = []
+        if os.path.exists(str(page)):
+            pass
+        else:
+            os.makedirs(str(page))
+#以页码为名称，创建文件夹
+
+        htmlinfo = gethtml(url)
+        piclist = re.findall('查看原图.*?"(.*?[jpgnif]{3})"', htmlinfo.text)
+#获取目标图片地址的列表
+
+        for picinfo in piclist:
+            if picinfo[:2]== '//':
+                picinfo = 'http:'+picinfo
+            if os.name == 'nt':
+                picpath = '.\\'+str(page)+'\\'
+            else:
+                picpath = './'+str(page)+'/'
+            picname = os.path.split(picinfo)[1]
+            htmlinfo= gethtml(picinfo,True)
+            picinfolist.append([picpath,picname,htmlinfo])
+        return picinfolist
+#get picpath, pathname, pathinfo and so on       
+
+if __name__ == '__main__':
+    while True:
+        num = input('请输入页码范围，min-max，请输入数字，中间用-隔开,退出输入“Q”：')
+        if num == 'Q':
+            break
+        min = int(num.split('-')[0])
+        max = int(num.split('-')[1])+1
+        for page in range(min, max):
+            picinfolist = spiderman(page)
+            for x in picinfolist:
+                savepic(x[0],x[1],x[2])        
+        
